cc67ec40d33a376d259b7d1d574e39a0bfdb6b8b
Andrew Bailey <public7727@gmail.com>
2020-05-24 18:28:01 -0400

    update to blog setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,37 +1,5 @@
-import Router from 'next/router';
-import Link from 'next/link';
-import Head from 'next/head';
-import NProgress from 'nprogress';
+import '../styles/index.css'
 
-Router.events.on('routeChangeStart', (url) => {
-  console.log(`Loading: ${url}`);
-  NProgress.start();
-});
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Import CSS for nprogress */}
-        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
-      </Head>
-      <nav>
-        <style jsx>{`
-          a {
-            margin: 0 10px 0 0;
-          }
-        `}</style>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <a href="/non-existing">Non Existing Page</a>
-      </nav>
-      <Component {...pageProps} />
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
